98e29db3d90ed5c2af8057e97c4537addde4bd73
Rajnish. R. Singh Rajput <srajnish235.rs@gmail.com>
2026-01-05 11:04:03 +0530

    added text splitter for the chucking

--- Backend/indexing.js
+++ Backend/indexing.js
@@ -1,9 +1,18 @@
 import { PDFLoader } from "@langchain/community/document_loaders/fs/pdf";
+import { RecursiveCharacterTextSplitter } from "@langchain/textsplitters";
+
 const FileForIndexing = "./Hybrid_Fitness_System_2026_Full.pdf";
 
 async function indexingFunction() {
+  // step 1: Load the PDF document
   const loader = new PDFLoader(FileForIndexing);
   const docs = await loader.load();
-  console.log("Documents loaded for indexing:", docs);
+
+  // Step 2: Split the loaded documents into smaller chunks.
+  const splitter = new RecursiveCharacterTextSplitter({
+    chunkSize: 100,
+    chunkOverlap: 0,
+  });
+  const splitDocs = await splitter.splitDocuments(docs);
 }
 indexingFunction();
