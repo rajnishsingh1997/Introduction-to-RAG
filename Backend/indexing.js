cee79a3e030072e3e5c683f887b2d024eac1ba51
Rajnish. R. Singh Rajput <srajnish235.rs@gmail.com>
2026-01-05 11:46:45 +0530

    Added collection and vector embedding for the pdf

--- Backend/indexing.js
+++ Backend/indexing.js
@@ -1,5 +1,7 @@
 import { PDFLoader } from "@langchain/community/document_loaders/fs/pdf";
 import { RecursiveCharacterTextSplitter } from "@langchain/textsplitters";
+import { QdrantVectorStore } from "@langchain/qdrant";
+import { OpenAIEmbeddings } from "@langchain/openai";
 
 const FileForIndexing = "./Hybrid_Fitness_System_2026_Full.pdf";
 
@@ -14,5 +16,23 @@ async function indexingFunction() {
     chunkOverlap: 0,
   });
   const splitDocs = await splitter.splitDocuments(docs);
+
+  const embeddings = new OpenAIEmbeddings({
+    model: "text-embedding-3-small",
+  });
+
+  const embeddingCollection = await QdrantVectorStore.fromDocuments(
+    splitDocs,
+    embeddings,
+    {
+      url: "http://localhost:6333",
+      collectionName: "embedding-collection",
+    }
+  );
+
+  //   const vectorStore = await QdrantVectorStore.fromExistingCollection(embeddings, {
+  //   url: process.env.QDRANT_URL,
+  //   collectionName: "langchainjs-testing",
+  // });
 }
 indexingFunction();
